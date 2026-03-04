a1c83790b4559d501c37d7ce863c8705ae7949ef
uff20xd <uff20xd@proton.me>
2026-03-04 17:13:55 +0100

    k

--- lua/yugener/theme.lua
+++ lua/yugener/theme.lua
@@ -74,7 +74,7 @@ function M.get(config)
     VertSplit = { fg = groups.border },
     Visual = { fg = p.color200, bg = p.color600 },
     WarningMsg = { fg = p.warning },
-    Whitespace = { fg = p.color700 },
+    Whitespace = { fg = p.color500 },
     WildMenu = { link = 'IncSearch' },
 
     Constant = { fg = p.color200 },
